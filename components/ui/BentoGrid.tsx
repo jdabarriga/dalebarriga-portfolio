d8dd4d5d1257e128baa0fc4e95024af1bfdc5b48
Dale Barriga <127873937+reyylaxed@users.noreply.github.com>
2025-03-21 18:10:25 -0600

    fixed text hover effect

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -21,7 +21,6 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        // change gap-4 to gap-8, change grid-cols-3 to grid-cols-5, remove md:auto-rows-[18rem], add responsive code
         "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
@@ -36,7 +35,6 @@ export const BentoGridItem = ({
   id,
   title,
   description,
-  //   remove unecessary things here
   img,
   imgClassName,
   titleClassName,
@@ -52,14 +50,14 @@ export const BentoGridItem = ({
   spareImg?: string;
 }) => {
   const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const rightLists = ["MongoDB", "MySQL", "Figma"];
 
   const [copied, setCopied] = useState(false);
 
   const defaultOptions = {
     loop: copied,
     autoplay: copied,
-    //animationData: animationData,
+    animationData: animationData,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
@@ -113,7 +111,7 @@ export const BentoGridItem = ({
         <div
           className={cn(
             titleClassName,
-            "group-hover bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
@@ -127,8 +125,8 @@ export const BentoGridItem = ({
           {id === 2 && <GridGlobe />}
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-              <div className="flex flex-col gap-3 lg:gap-8">
-                {["React.js", "Next.js", "TypeScript"].map((item) => (
+              <div className="flex flex-col gap-3 lg:gap-5">
+                {leftLists.map((item) => (
                   <span
                     key={item}
                     className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-90 rounded-xl text-center bg-[#10132E]"
@@ -138,9 +136,9 @@ export const BentoGridItem = ({
                 ))}
                 <span className="py-4 px-3 rounded-xl text-center bg-[#10132E]" />
               </div>
-              <div className="flex flex-col gap-3 lg:gap-8">
+              <div className="flex flex-col gap-3 lg:gap-5">
                 <span className="py-4 px-3 rounded-xl text-center bg-[#10132E]" />
-                {["MongoDB", "MySQL", "TailwindCSS"].map((item) => (
+                {rightLists.map((item) => (
                   <span
                     key={item}
                     className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-90 rounded-xl text-center bg-[#10132E]"
@@ -179,30 +177,3 @@ export const BentoGridItem = ({
     </div>
   );
 };
-
-// {id === 3 && (
-//   <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-//     <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-//       {leftLists.map((item, i) => (
-//         <span
-//           key={i}
-//           className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-//         >
-//           {item}
-//         </span>
-//       ))}
-//       <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#10132E]"></span>
-//     </div>
-//     <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-//       <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-[#10132E]"></span>
-//       {rightLists.map((item, i) => (
-//         <span
-//           key={i}
-//           className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-//         >
-//           {item}
-//         </span>
-//       ))}
-//     </div>
-//   </div>
-// )}
